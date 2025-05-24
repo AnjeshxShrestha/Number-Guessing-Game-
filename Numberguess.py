b9d4398197e0f3b4d5acc83f8da47ea49110b4bf
AnjeshxShrestha <anjeshshrestha039@gmail.com>
2025-05-24 21:35:11 +0545

    Update Numberguess.py

--- Numberguess.py
+++ Numberguess.py
@@ -1,15 +1,11 @@
 import random
-
 def number_guessing_game():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
-
     secret_number = random.randint(1, 100)
     attempts = 0
-
     while True:
         guess = input("Enter your guess: ")
-
         if not guess.isdigit():
             print("Please enter a valid number.")
             continue
